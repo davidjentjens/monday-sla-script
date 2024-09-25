17ae9ff1d25ef2457a066fe2460e8a249d7f0887
David M. Jentjens <davidjentjens@gmail.com>
2024-09-25 09:32:32 -0300

    Corrigir horário de trabalho de 8-17 para 8-18

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ import { startBusinessHours, stopBusinessHours } from './helpers/timers'
 
 console.log('[SETUP] Initializing cron jobs...\n')
 
-// Schedule the tasks to run at 8 AM and 5 PM on weekdays
+// Schedule the tasks to run at 8 AM and 6 PM on weekdays
 cron.schedule('0 8 * * 1-5', startBusinessHours, {
   timezone: 'America/Sao_Paulo',
 })
 
-cron.schedule('0 17 * * 1-5', stopBusinessHours, {
+cron.schedule('0 18 * * 1-5', stopBusinessHours, {
   timezone: 'America/Sao_Paulo',
 })
 
